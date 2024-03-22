785101f05bd7a81ebd55754df6546c20428c5a24
Kalpesh_23 <kalpeshkolambe23@gmail.com>
2024-03-22 13:38:06 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,103 @@
+# import streamlit as st 
+# import pickle
+
+
+
+# tfidf = pickle.load(open('vectorizer.pkl','rb'))
+# model = pickle.load(open('model.pkl','rb'))
+
+# st.title('Email/SMS Spam Classifier')
+
+# input_sms = st.text_area('Enter the message')
+
+# #Preprocessing data
+# def tranform_text(text):
+#     text = text.lower()  #lower case
+#     text = nltk.word_tokenize(text)  # tokenization
+    
+#     # removing Special Characters
+#     y = []
+#     for i in text:
+#         if i.isalnum():
+#             y.append(i)
+#     text = y[:]
+#     y.clear()
+    
+#     # remove stopwords and punctuation:
+#     for i in text:
+#         if i not in stopwords.words('english') and i not in string.punctuation:
+#             y.append(i)
+#     text = y[:]
+#     y.clear()
+    
+#     #Stemming
+#     for i in text:
+#         ps = PorterStemmer()
+#         y.append(ps.stem(i))
+#     return " ".join(y)
+
+# transform_sms = tranform_text(input_sms)
+
+# vector_input = tfidf.transform([transform_sms])
+
+# result = model.predict(vector_input)[0]
+
+# if result == 1:
+#     st.header('spam')
+# else:
+#     st.header('not spam')
+
+import streamlit as st 
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import string
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title('Email/SMS Spam Classifier')
+
+input_sms = st.text_area('Enter the message')
+
+#Preprocessing data
+def transform(text):
+    text = text.lower()  #lower case
+    text = nltk.word_tokenize(text)  # tokenization
+    
+    # removing Special Characters
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    
+    # remove stopwords and punctuation:
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    text = y[:]
+    y.clear()
+    
+    #Stemming
+    for i in text:
+        ps = PorterStemmer()
+        y.append(ps.stem(i))
+    return " ".join(y)
+
+if st.button('Predict'):
+    transformed_sms = transform(input_sms)
+
+    vector_input = tfidf.transform([transformed_sms])
+
+    result = model.predict(vector_input)[0]
+
+    if result == 1:
+        st.header('The Message is Spam')
+    else:
+        st.header('The Message is not spam')
+
+
+
---
+++ readme.md
@@ -0,0 +1,69 @@
+# SMS/Email Spam Prediction
+
+
+UI of Application:-
+
+Application URL Links : 
+
+
+
+## Table of contents
+* [About project]
+* [Technologies]
+* [Software and account requirement]
+* [Project Pipeline]
+
+
+## About project
+SMS spam classifier is a machine learning model designed to identify and distinguish between spam and legitimate text messages.
+
+
+
+## Technologies
+This project is created with below technologies/tools/resources:
+* Python: 3.7
+* Jupyter Notebook
+* Machine Learning
+* Streamlit
+* Git
+
+
+## Software and account Requirement
+1. [Github Account]
+2. [Streamlit Account]
+3. [VS Code IDE]
+
+
+## Project Pipeline
+1. [Data Cleaning]
+2. [Data EDA]
+3. [Data Preprocessing]
+4. [Model Building]
+5. [Model Evaluation]
+6. [Model Deployement]
+
+1. Data Cleaning : 
+* Process of identifying and correcting errors or inconsistencies in data to improve its quality and reliability.
+
+2. Data EDA :
+* Analyzing and visualizing data to understand its characteristics, uncover patterns, and identify relationships between variables.
+
+3. Data Preprocessing : 
+* Preparing and transforming raw data into a format suitable for analysis, including handling missing values, encoding categorical variables, and scaling numerical features.
+
+4. Model Building :
+* Creating and training a machine learning model using the preprocessed data to make predictions or classifications based on input features.
+
+5. Model Evaluation
+* Model evaluation is the process of using different evaluation metrics to understand a machine learning model’s performance, as well as its strengths and weaknesses.
+* Model evaluation is important to assess the efficacy of a model during initial research phases, and it also plays a role in model monitoring.
+
+6. Model Deployment
+* Deployment is the method by which we integrate a machine-learning model into the production environment to make practical business decisions based on data. 
+
+
+  
+  
+Thanks & Regards
+Kalpesh Kolambe
+  
